ea76d4e41090aa33cd48927b5158677f36f01d20
Johannes Schmidt <devshred@tigerflanke.de>
2023-03-17 14:12:34 +0100

    add sample solution

--- src/main/java/org/example/bowling/CyberBowlORama.java
+++ src/main/java/org/example/bowling/CyberBowlORama.java
@@ -1,3 +1,36 @@
 package org.example.bowling;
 
-public class CyberBowlORama implements BowlingGame {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class CyberBowlORama implements BowlingGame {
+    private final List<Integer> rolls = new ArrayList<>();
+
+    @Override
+    public void roll(int pins) {
+        rolls.add(pins);
+    }
+
+    @Override
+    public int score() {
+        int rollIndex = 0;
+        int sum = 0;
+
+        for (int frameIndex = 0; frameIndex < 10; frameIndex++) {
+            int firstRoll = rolls.get(rollIndex++);
+            sum += firstRoll;
+
+            if (firstRoll == 10) {
+                sum += rolls.get(rollIndex) + rolls.get(rollIndex + 1);
+            } else {
+                int secondRoll = rolls.get(rollIndex++);
+                sum += secondRoll;
+                if (firstRoll + secondRoll == 10) {
+                    sum += rolls.get(rollIndex);
+                }
+            }
+        }
+
+        return sum;
+    }
+}
--- src/test/java/org/example/bowling/CyberBowlORamaTest.java
+++ src/test/java/org/example/bowling/CyberBowlORamaTest.java
@@ -5,20 +5,68 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 class CyberBowlORamaTest {
+    private final BowlingGame game = new CyberBowlORama();
+
     @Test
     void testGutterGame() {
-        var game = new CyberBowlORama();
-        for (int i = 0; i < 10 * 2; i++) {
-            game.roll(0);
-        }
+        rollMany(10 * 2, 0);
         assertThat(game.score()).isZero();
     }
 
-    // testAllOnes
+    @Test
+    void testAllOnes() {
+        rollMany(10 * 2, 1);
+        assertThat(game.score()).isEqualTo(20);
+    }
 
-    // testOneSpare
+    @Test
+    void testOneSpare() {
+        // 1st frame
+        rollSpare();
 
-    // testOneStrike
+        // 2nd frame
+        game.roll(3);
+        game.roll(5);
 
-    // testPerfectGame
+        rollMany(8 * 2, 0);
+        assertThat(game.score()).isEqualTo(21);
+    }
+
+    @Test
+    void testOneStrike() {
+        // 1st frame
+        rollStrike();
+
+        // 2nd frame
+        game.roll(3);
+        game.roll(2);
+
+        // 3rd frame
+        game.roll(1);
+        game.roll(4);
+
+        rollMany(7 * 2, 0);
+        assertThat(game.score()).isEqualTo(25);
+    }
+
+    @Test
+    void testPerfectGame() {
+        rollMany(12, 10);
+        assertThat(game.score()).isEqualTo(300);
+    }
+
+    private void rollMany(int number, int pins) {
+        for (int i = 0; i < number; i++) {
+            game.roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        game.roll(8);
+        game.roll(2);
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
 }
